c03e15390f4306002845a4721553f757bfa6504f
a4koshin <mankajr11@gmail.com>
2025-12-26 21:14:24 +0300

    Changing the file extension

---
+++ lib/navigation.ts
@@ -0,0 +1,28 @@
+interface Navbar {
+  id: number;
+  name: string;
+  href: string;
+}
+
+export const navbar: Navbar[] = [
+  {
+    id: 0,
+    name: "Hem",
+    href: "/",
+  },
+  {
+    id: 1,
+    name: "Om oss",
+    href: "/omoss",
+  },
+  {
+    id: 2,
+    name: "Våra tjänster",
+    href: "/våratjänster",
+  },
+  {
+    id: 3,
+    name: " Kontakta oss",
+    href: "/kontaktaoss",
+  },
+];
