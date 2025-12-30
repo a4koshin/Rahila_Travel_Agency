e2eb0d2c66c871a31240499afe1a05d980ad3b08
a4koshin <mankajr11@gmail.com>
2025-12-30 15:14:41 +0300

    Osmos page is added the heading of the page

--- app/omoss/page.tsx
+++ app/omoss/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const AboutPage = () => {
+  return (
+    <div className="bg-amber-50">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div className="mb-12 text-center">
+          <h1 className="font-bold text-2xl mb-4 text-blue-950">
+            About us page
+          </h1>
+          <p className="text-md text-amber-500">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque in,
+            amet nam quam fugit dolorum esse vel dolore sit.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default page;
+export default AboutPage;
