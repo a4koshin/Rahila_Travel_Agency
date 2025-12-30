36c6ef178109f51a6af8193dc3f3d14915b15c45
a4koshin <mankajr11@gmail.com>
2025-12-30 16:50:53 +0300

    What we do list data

---
+++ lib/whatWeDo.ts
@@ -0,0 +1,37 @@
+interface WhatWeDo {
+  //   id: number;
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export const whatWeDo: WhatWeDo[] = [
+  {
+    icon: "🌴",
+    title: "Tailor-Made Holiday Packages",
+    description:
+      "Beach getaways, cultural experiences, adventure trips, and much more personalized to your preferences",
+  },
+  {
+    icon: "✈️",
+    title: "Flight & Accommodation Bookings",
+    description:
+      "Competitive prices and smooth arrangements for all your travel needs",
+  },
+  {
+    icon: "👥",
+    title: "Group & Corporate Travel",
+    description: "Efficient and reliable planning for teams and organizations",
+  },
+  {
+    icon: "📄",
+    title: "Visa Assistance & Documentation",
+    description: "Professional support that simplifies the entire process",
+  },
+  {
+    icon: "🕌",
+    title: "Specialized Travel",
+    description:
+      "Spiritual pilgrimages, luxury travel, and carefully curated itineraries",
+  },
+];
